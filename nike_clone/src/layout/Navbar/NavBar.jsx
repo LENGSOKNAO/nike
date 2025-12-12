a11da28e6200e817d5649620aa05f9f299917e11
soknao <soknao2003@gmail.com>
2025-12-12 21:30:21 +0700

    sok

---
+++ nike_clone/src/layout/Navbar/NavBar.jsx
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import { Link } from "react-router";
+import { navList } from "../../model/ListNav";
+import { CiBag1, CiHeart, CiSearch } from "react-icons/ci";
+import { Menu } from "lucide-react";
+
+const NavBar = () => {
+  const [nav, setNav] = useState(false);
+
+  return (
+    <header>
+      {/* screen md */}
+      <div className="lg:hidden">
+        <nav className="flex items-center  justify-between px-[5%] ">
+          {/* rigth */}
+          <Link>
+            {/* logo */}
+            <img src="src/assets/logo.png" className="text-black" alt="" />
+          </Link>
+
+          {/* left */}
+          <div className="flex items-center gap-2 text-2xl">
+            <CiSearch />
+            <CiHeart />
+            <CiBag1 />
+            <Menu onClick={() => setNav((e) => !e)} />
+            {nav && (
+              <div className="">
+                <ul>
+                  {navList.map((e, i) => (
+                    <>
+                      <li> {e.mainLink} </li>
+                    </>
+                  ))}
+                </ul>
+              </div>
+            )}
+          </div>
+        </nav>
+      </div>
+      {/* screen full */}
+      <div className="hidden sm:hidden md:hidden lg:block">
+        <nav className="flex items-center  justify-between px-[5%] ">
+          {/* rigth */}
+          <Link>
+            {/* logo */}
+            <img src="src/assets/logo.png" className="text-black" alt="" />
+          </Link>
+          {/* center  */}
+          <div className="">
+            <ul className="flex gap-5 text-md font-semibold">
+              {navList.map((e, i) => (
+                <>
+                  <li key={i}>
+                    <Link to={e.link}> {e.mainLink} </Link>
+                  </li>
+                </>
+              ))}
+            </ul>
+          </div>
+          {/* left */}
+          <div className="flex items-center gap-2 text-2xl">
+            <CiSearch />
+            <CiHeart />
+            <CiBag1 />
+          </div>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default NavBar;
