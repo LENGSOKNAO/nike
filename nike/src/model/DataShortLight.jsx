1abac119695e6afdcea44a265bfcf6120b741fbb
soknao <soknao2003@gmail.com>
2025-12-14 23:50:15 +0700

    sok

---
+++ nike/src/model/DataShortLight.jsx
@@ -0,0 +1,82 @@
+export const listDataShortLight = [
+  {
+    image: "/src/assets/listSportLight/image.png",
+    title: "Fan Gear",
+    links: "",
+  },
+  {
+    image: "/src/assets/listSportLight/image1.png",
+    title: "Jordan Retro",
+    links: "",
+  },
+  {
+    image: "/src/assets/listSportLight/image2.png",
+    title: "Air Force1",
+    links: "",
+  },
+  {
+    image: "/src/assets/listSportLight/image3.png",
+    title: "24.7 Collection",
+    links: "",
+  },
+  {
+    image: "/src/assets/listSportLight/image4.png",
+    title: "Tatum 4",
+    links: "",
+  },
+  {
+    image: "/src/assets/listSportLight/image5.png",
+    title: "Vomero 5",
+    links: "",
+  },
+  {
+    image: "/src/assets/listSportLight/image7.png",
+    title: "Rejuven8",
+    links: "",
+  },
+  {
+    image: "/src/assets/listSportLight/image8.png",
+    title: "Pegasus Premium",
+    links: "",
+  },
+  {
+    image: "/src/assets/listSportLight/image9.png",
+    title: "ACG",
+    links: "",
+  },
+  {
+    image: "/src/assets/listSportLight/image22.png",
+    title: "Vomero Plus",
+    links: "",
+  },
+  {
+    image: "/src/assets/listSportLight/image44.png",
+    title: "Graphic Tees",
+    links: "",
+  },
+  {
+    image: "/src/assets/listSportLight/image55.png",
+    title: "Dunk",
+    links: "",
+  },
+  {
+    image: "/src/assets/listSportLight/image99.png",
+    title: "Lebron XXLLL",
+    links: "",
+  },
+  {
+    image: "/src/assets/listSportLight/image66.png",
+    title: "Ari Jordan 1",
+    links: "",
+  },
+  {
+    image: "/src/assets/listSportLight/image6.png",
+    title: "Shox",
+    links: "",
+  },
+  {
+    image: "/src/assets/listSportLight/33image.png",
+    title: "Ari Max",
+    links: "",
+  },
+];
