aa33069ea8fa57df4c565b14fb593018122da994
seyha mai <seyhaamai40@gmail.com>
2025-12-18 21:08:57 +0700

    sey

--- nike/src/components/men/SportMen.jsx
+++ nike/src/components/men/SportMen.jsx
@@ -1,19 +1,50 @@
 import React from "react";
 import { MdOutlineArrowBackIosNew } from "react-icons/md";
 import { GrNext } from "react-icons/gr";
+import { Link } from "react-router";
+import BannerCart from "../props/cart/BannerCart";
 
+const IconButton = ({ children }) => (
+  <button
+    className="w-10 h-10 flex items-center justify-center rounded-full 
+                     bg-gray-100 hover:bg-gray-200 transition"
+  >
+    {children}
+  </button>
+);
+const Sport = [
+  { img: "/src/assets/menpicsey/slider/sp1.jpg", namebtn: "shop", link: "" },
+  { img: "/src/assets/menpicsey/slider/sp2.jpg", namebtn: "shop", link: "" },
+  { img: "/src/assets/menpicsey/slider/sp3.jpg", namebtn: "shop", link: "" },
+  { img: "/src/assets/menpicsey/slider/sp4.jpg", namebtn: "shop", link: "" },
+  { img: "/src/assets/menpicsey/slider/sp5.jpg", namebtn: "shop", link: "" },
+];
 const SportMen = () => {
   return (
     <>
       <div className="flex justify-between px-20 mt-10">
         <h2>Shop by Sport </h2>
         <div className="flex gap-5 ">
-          <iconbutton className="w-10 h-10 flex justify-center items-center bg-gray-100 rounded-full ">
-            <MdOutlineArrowBackIosNew />{" "}
-          </iconbutton>
-          <iconbutton className="w-10 h-10 flex justify-center items-center bg-gray-100 rounded-full ">
+          <IconButton>
+            <MdOutlineArrowBackIosNew />
+          </IconButton>
+          <IconButton>
             <GrNext />
-          </iconbutton>
+          </IconButton>
+        </div>
+      </div>
+      <div className="">
+        <div className="">
+          {Sport.map((e) => {
+            <div>
+              <BannerCart
+                image={e.img}
+                smallText=""
+                bigText=""
+                nameBtn={e.namebtn}
+              />
+            </div>;
+          })}
         </div>
       </div>
     </>
--- nike/src/screen/men/MenHome.jsx
+++ nike/src/screen/men/MenHome.jsx
@@ -3,13 +3,14 @@ import React from "react";
 import MenBar from "../../components/men/MenBar";
 import HeaderMen from "../../components/men/HeaderMen";
 import SportMen from "../../components/men/SportMen";
-
+// import Test from "../../components/men/test";
 const MenHome = () => {
   return (
     <Layout>
       <MenBar />
       <HeaderMen />
       <SportMen />
+      {/* <Test /> */}
     </Layout>
   );
 };
