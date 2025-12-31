a4a1fcea4c216e3281f1ce1130b0262e6ddd4cca
soknao <soknao2003@gmail.com>
2025-12-31 15:45:55 +0700

    sok

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import MenHome from "./screen/men/MenHome";
 import WomenHome from "./screen/women/womenHome";
 import Sale from "./screen/sale/Sale";
 import Test from "./components/men/Test";
+import UsingProps from "./testing/UsingProps";
 
 const App = () => {
   return (
@@ -14,7 +15,7 @@ const App = () => {
         <Route path="/men" element={<MenHome />} />
         <Route path="/women" element={<WomenHome />} />
         <Route path="/sale" element={<Sale />} />
-        <Route path="/testing" element={<Test />} />
+        <Route path="/testing" element={<UsingProps />} />
       </Routes>
     </BrowserRouter>
   );
