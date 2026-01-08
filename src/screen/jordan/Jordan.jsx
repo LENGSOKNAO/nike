3c7ba55c9e35aed5a0402538742d53ce70fcf512
seyhamai <seyhaamai40@gmail.com>
2026-01-08 21:33:01 +0700

    sey

--- src/screen/jordan/Jordan.jsx
+++ src/screen/jordan/Jordan.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Layout from "../../layout/Layout.jsx";
 import LinkPage from "../../components/props/LinkPage.jsx";
+import NavbarJordan from "../../components/jordan/NavbarJordan.jsx";
 
 const Jordan = () => {
   return (
     <div className="">
       <Layout bg={true} text={"text-white"}>
         <LinkPage color={true} />
+        <div className="bg-jordan w-full h-auto text-white">
+          <NavbarJordan />
+        </div>
       </Layout>
     </div>
   );
